487b903d39cf09156ce95d55afa3ed0d09cef2d9
kevk <kevinku668833@gmail.com>
2024-09-09 20:57:41 -0700

    added general styling to homepage and fixed form warnings

--- src/components/Home/GetInTouch.js
+++ src/components/Home/GetInTouch.js
@@ -8,11 +8,11 @@ function GetInTouch() {
       <form>
         <fieldset className={styles.content}>
           <legend><em>Send A Message</em></legend>
-          <label for="name-area">Name:</label>
-          <input type="text" id="name-area" placeholder="Name" pattern="^[a-zA-Z ]*$" maxlength="256" required="" class="invalid" />
-          <label for="mail">Email:</label>
-          <input type="email" placeholder="Email" id="mail" minlength="3" name="mail" maxlength="254" required="" class="valid" />
-          <label for="message-area">Message:</label>
+          <label htmlFor="name-area">Name:</label>
+          <input type="text" id="name-area" placeholder="Name" pattern="^[a-zA-Z ]*$" maxLength="256" required="" className="invalid" />
+          <label htmlFor="mail">Email:</label>
+          <input type="email" placeholder="Email" id="mail" minLength="3" name="mail" maxLength="254" required="" className="valid" />
+          <label htmlFor="message-area">Message:</label>
           <textarea id="message-area" placeholder="Your Message..." required=""></textarea>
           <button type="submit">Send</button>
         </fieldset>
