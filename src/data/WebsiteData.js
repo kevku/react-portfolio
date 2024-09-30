4abcbf97ae80e670850c54c1f5b90e40fac03485
kevk <kevinku668833@gmail.com>
2024-09-30 02:55:53 -0700

    Added websites

--- src/data/WebsiteData.js
+++ src/data/WebsiteData.js
@@ -7,16 +7,16 @@ const websiteData = [
     overviewAlt: "Homepage of LeJournal",
     title: "Developer's Journal and Organizer",
     appName: "LeJournal",
-    text: "",
+    text: "In my Software Engineering course at UCSD, I led a team of 9 students to develop a CRUD application designed for developers. Our application focused on progressive enhancement and utilized local storage to save user input. The app allowed users to manage tasks, track events via a calendar, log daily code activity, store important documentation, and include an FAQ section for later reference. Throughout the project, we followed Agile development principles and modern software engineering practices such as continuous integration, quality-focused code development, and maintaining clean, organized codebases. We prioritized user-focused interfaces and ensured that our code, along with comprehensive documentation, could be easily maintained by any developer after completion. This class emphasized the application of theoretical concepts to real-world scenarios, exposing us to essential practices like shipping production-quality code, developing helpful documentation, and implementing rigorous testing. The course also highlighted the importance of collaboration and communication within a team, as well as delivering software that remains functional and maintainable beyond its initial development.",
     repoLink: "https://github.com/cse110-sp24-group8/cse110-sp24-group8",
     siteLink: "https://cse110-sp24-group8.github.io/cse110-sp24-group8/"
   },
   {
     overviewImg: HTMLSite,
     overviewAlt: "Homepage of HTML Site",
-    title: "First HTML, CSS, Javascript Site",
+    title: "Web Client Languages Course",
     appName: "Personal Portfolio",
-    text: "",
+    text: "In this course, I developed a foundational understanding of web and internet technologies, focusing on user-centric outcomes such as performance, accessibility, usability, security, and privacy. I learned about standards-compliant HTML and semantic markup, responsive design with CSS, and the fundamentals of JavaScript, including DOM manipulation, event handling, and client-server communication through APIs like Fetch and XMLHttpRequest. I also explored architectural patterns such as Static Sites, MPAs, and SPAs, and gained awareness of HTTP and client-side storage. For my project, I built a simple portfolio using HTML, CSS, and JavaScript, prioritizing accessibility, adherence to W3 standards, and progressive enhancement.",
     repoLink: "https://github.com/kevku/cse134-hw4",
     siteLink: "https://musical-figolla-6481b5.netlify.app/"
   }
